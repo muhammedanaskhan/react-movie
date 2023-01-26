c0770ac4f4f80bf7a9c6bf0600f614d6160f3727
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-01-26 08:33:53 -0800

    api object fetch

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../../axios';
+import requests from '../../Requests';
 import './Banner.css'
+
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => { //fetch data
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1) // gen rand no. from 0->len
+                ]
+            )
+            return request;
+        }
+        fetchData();
+    },[])
+
+    console.log(movie);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string
