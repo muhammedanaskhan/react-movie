0ec454d26bffdb2adacdcc7c3c72f6f4c2ca79ff
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-01-26 22:37:05 -0800

    getting particular genre movies data at each row

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,12 +2,27 @@ import React from 'react'
 import './HomeScreen.css'
 import Nav from './Nav/Nav'
 import Banner from './Banner/Banner'
+import requests from '../Requests'
+import Row from './Row/Row'
 
 function HomeScreen() {
   return (
     <div className='homescreen'>
         <Nav/>
         <Banner/>
+        <Row 
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+        <Row title="Trending Now" fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    
     </div>
   )
 }
