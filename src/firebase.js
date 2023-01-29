000ee2aa80e25b05434076258ae7d782346e745e
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-01-29 04:14:14 -0800

    signin and login implemntd

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,9 @@
+// import firebase from 'firebase';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCcEp8urf9OTGGE8SDdHPJN904oSuQYlDM",
   authDomain: "react-movie-ae1df.firebaseapp.com",
@@ -6,3 +12,10 @@ const firebaseConfig = {
   messagingSenderId: "774195753698",
   appId: "1:774195753698:web:0e316224090551b2f2a4bb"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore()
+const auth = firebase.auth();
+
+export {auth}
+export default db;
